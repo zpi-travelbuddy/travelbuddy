67448bfa79ecf9ece1ddc52409b2b830aff35e74
Bartosz Gableta <aksiorex@gmail.com>
2024-10-22 01:41:18 +0200

    Move font config to separate file

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -4,6 +4,7 @@ import {
   lightThemeHighContrast,
   darkThemeHighContrast,
 } from "@/constants/Colors";
+import { FontConfigs } from "@/constants/FontConfigs";
 import {
   Manrope_400Regular,
   Manrope_500Medium,
@@ -13,60 +14,9 @@ import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { ThemeProp } from "react-native-paper/lib/typescript/types";
 
-const fontConfig = {
-  default: {
-    fontFamily: "Manrope_400Regular",
-  },
-  bodySmall: {
-    fontFamily: "Manrope_400Regular",
-  },
-  bodyMedium: {
-    fontFamily: "Manrope_400Regular",
-  },
-  bodyLarge: {
-    fontFamily: "Manrope_400Regular",
-  },
-  headlineSmall: {
-    fontFamily: "Manrope_400Regular",
-  },
-  headlineMedium: {
-    fontFamily: "Manrope_400Regular",
-  },
-  headlineLarge: {
-    fontFamily: "Manrope_400Regular",
-  },
-  labelSmall: {
-    fontFamily: "Manrope_500Medium",
-  },
-  labelMedium: {
-    fontFamily: "Manrope_500Medium",
-  },
-  labelLarge: {
-    fontFamily: "Manrope_500Medium",
-  },
-  titleSmall: {
-    fontFamily: "Manrope_500Medium",
-  },
-  titleMedium: {
-    fontFamily: "Manrope_500Medium",
-  },
-  titleLarge: {
-    fontFamily: "Manrope_500Medium",
-  },
-  displaySmall: {
-    fontFamily: "Manrope_400Regular",
-  },
-  displayMedium: {
-    fontFamily: "Manrope_400Regular",
-  },
-  displayLarge: {
-    fontFamily: "Manrope_400Regular",
-  },
-};
-
 const theme: ThemeProp = {
   ...lightThemeHighContrast,
-  fonts: configureFonts({ config: fontConfig }),
+  fonts: configureFonts({ config: FontConfigs.manrope }),
 };
 
 SplashScreen.preventAutoHideAsync();
