cc41d1c809879bc0651e2a9e7ea07c6efa2b46a1
AdrianBroniecki <266603@student.pwr.edu.pl>
2024-10-22 17:13:30 +0200

    [ZPI-17] Defining colors in app #1 (#9)
    Co-authored-by: Bartosz Gableta <aksiorex@gmail.com>

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,9 +1,51 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
+import { Themes } from "@/constants/Themes";
+import {
+  Manrope_400Regular,
+  Manrope_500Medium,
+  useFonts,
+} from "@expo-google-fonts/manrope";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { useMemo, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const systemTheme = useColorScheme();
+
+  const [loaded, error] = useFonts({
+    Manrope_400Regular,
+    Manrope_500Medium,
+  });
+
+  const theme = useMemo(() => {
+    switch (systemTheme) {
+      case "dark":
+        return Themes.darkTheme;
+      case "light":
+      default:
+        return Themes.lightTheme;
+    }
+  }, [systemTheme]);
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <PaperProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </PaperProvider>
   );
 }
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,12 +1,17 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 
 export default function Index() {
+  const theme = useTheme();
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: theme.colors.background,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
