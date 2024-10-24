039ac9f159e46f0a10c9158538da761cdc34d7a6
Mateusz Gazda <matigazda29@gmail.com>
2024-10-24 17:03:03 +0200

    Add FullName to PlaceCategory and PlaceCondition

--- backend/TravelBuddyAPI/Models/PlaceCondition.cs
+++ backend/TravelBuddyAPI/Models/PlaceCondition.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TravelBuddyAPI.Models;
 
@@ -11,6 +12,9 @@ public class PlaceCondition
     [Required]
     public string? Name { get; set; }
 
+    [NotMapped]
+    public string? FullName => GetFullName(this);
+
     public string? Description { get; set; }
 
     public Guid? SuperConditionId { get; set; }
@@ -19,4 +23,20 @@ public class PlaceCondition
     public List<PlaceCondition>? SubConditions { get; set; }
     public List<ConditionProfile>? ConditionProfiles { get; internal set; }
     public List<ProviderPlace>? ProviderPlaces { get; internal set; }
+
+    private static string? GetFullName(PlaceCondition condition)
+    {
+        if (condition.SuperConditionId == null)
+        {
+            return condition.Name;
+        }
+
+        if (condition.SuperCondition == null)
+        {
+            return null;
+        }
+
+        var superConditionFullName = GetFullName(condition.SuperCondition);
+        return superConditionFullName is null ? null : $"{superConditionFullName}.{condition.Name}";
+    }
 }
