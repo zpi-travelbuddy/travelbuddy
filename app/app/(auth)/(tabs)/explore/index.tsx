25e64291cdbc099a4a0cba69465f93042cf012aa
Bartosz Gableta <aksiorex@gmail.com>
2024-10-24 18:22:37 +0200

    Create basic Explore screen

--- app/app/(auth)/(tabs)/explore/index.tsx
+++ app/app/(auth)/(tabs)/explore/index.tsx
@@ -1,20 +1,50 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
-import { useTheme } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+import { Searchbar } from "react-native-paper";
+import { MD3ThemeExtended } from "@/constants/Themes";
 
 export default function Explore() {
-  const theme = useTheme();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [places, setPlaces] = useState([]);
+
+  const theme = useTheme() as MD3ThemeExtended;
+  const styles = makeStyles(theme);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: theme.colors.background,
-      }}
-    >
-      <Text>Explore</Text>
+    <View style={styles.container}>
+      <Searchbar
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        placeholder="Wyszukaj atrakcję"
+        style={styles.searchbar}
+      />
+      {places.length > 0 ? (
+        "test"
+      ) : (
+        <Text style={styles.text} variant="bodyLarge">
+          Użyj wyszukiwania aby przeglądać dostępne atrakcje
+        </Text>
+      )}
     </View>
   );
 }
+
+const makeStyles = (theme: MD3ThemeExtended) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      paddingHorizontal: 20,
+      backgroundColor: theme.colors.background,
+    },
+    searchbar: {
+      alignSelf: "stretch",
+      backgroundColor: theme.colors.surfaceContainerHigh,
+      marginVertical: 20,
+    },
+    text: {
+      textAlign: "center",
+    },
+  });
