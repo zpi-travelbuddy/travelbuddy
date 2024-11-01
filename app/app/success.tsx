ef8589cbbebcdeccdadb48f8143a69a2031b32e7
Bartosz Gableta <aksiorex@gmail.com>
2024-11-01 11:00:37 +0100

    Change text Link to Button for Confirm screen

--- app/app/success.tsx
+++ app/app/success.tsx
@@ -5,50 +5,38 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import { Link } from "expo-router";
-import { useTheme, Text } from "react-native-paper";
-import Animated, {
-  useAnimatedKeyboard,
-  useAnimatedStyle,
-} from "react-native-reanimated";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Link, router } from "expo-router";
+import { useTheme, Text, Button } from "react-native-paper";
 import { MD3ThemeExtended } from "@/constants/Themes";
-
-// It would be good if we could calculate this value dynamically, but I had some issues with that
-const BOTTOM_VIEW_HEIGHT = 54;
 
 export default function Confirmation() {
   const theme = useTheme() as MD3ThemeExtended;
-  const insets = useSafeAreaInsets();
-  const keyboard = useAnimatedKeyboard();
 
   const styles = makeStyles(theme);
 
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      marginBottom: Math.max(
-        keyboard.height.value - BOTTOM_VIEW_HEIGHT - insets.bottom,
-        0,
-      ),
-    };
-  });
+  const handlePress = () => {
+    router.navigate("/sign-in");
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.innerContainer}>
-          <Animated.View style={[animatedStyles]}>
-            <Text style={styles.headline} variant="headlineLarge">
-              Udało się!
-            </Text>
-            <Text variant="bodyLarge" style={styles.paragraph}>
-              Potwierdzono założenie konta.{" \n"}
-              <Link href="/sign-in">
-                Wróć do{" "}
-                <Text style={{ fontFamily: "Manrope_700Bold" }}>logowania</Text>
-              </Link>
-            </Text>
-          </Animated.View>
+          <Text style={styles.headline} variant="headlineLarge">
+            Udało się!
+          </Text>
+          <Text variant="bodyLarge" style={styles.paragraph}>
+            Potwierdzono założenie konta.
+          </Text>
+          <Button
+            style={styles.button}
+            labelStyle={styles.buttonLabel}
+            mode="contained"
+            onPress={handlePress}
+            contentStyle={styles.buttonContent}
+          >
+            Wróć do logowania
+          </Button>
         </View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
@@ -72,6 +60,18 @@ const makeStyles = (theme: MD3ThemeExtended) =>
     },
     paragraph: {
       marginHorizontal: 40,
-      marginBottom: 240,
+      marginBottom: 140,
+    },
+    button: {
+      alignSelf: "stretch",
+      marginHorizontal: 40,
+      marginBottom: 84,
+      marginTop: 40,
+    },
+    buttonLabel: {
+      fontSize: 16,
+    },
+    buttonContent: {
+      paddingVertical: 0,
     },
   });
