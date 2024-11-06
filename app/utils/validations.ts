0ddafef3c8a8fb075eaa4847d4c707934167bafc
Bartosz Gableta <51949922+bartoszgableta@users.noreply.github.com>
2024-11-06 21:10:42 +0100

    [ZPI-44] Create signup view (#51)

--- app/utils/validations.ts
+++ app/utils/validations.ts
@@ -2,3 +2,11 @@ export const validateEmail = (email: string) => {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 };
+
+export const validateField = (field: "email" | "password", value: string) => {
+  if (!value)
+    return `${field === "email" ? "Email jest wymagany" : "Hasło jest wymagane"}`;
+  if (field === "email" && !validateEmail(value))
+    return "Niepoprawny format email";
+  return "";
+};
