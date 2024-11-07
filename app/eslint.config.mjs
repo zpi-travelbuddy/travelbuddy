8d55b32b39bf669ef8909c18c52f7f9ac305be63
AdrianBroniecki <266603@student.pwr.edu.pl>
2024-11-07 18:13:08 +0100

    Adding new rules

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -26,6 +26,7 @@ export default [
   eslintPluginPrettierRecommended,
   {
     rules: {
+      "linebreak-style": "off",
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "prettier/prettier": "error",
