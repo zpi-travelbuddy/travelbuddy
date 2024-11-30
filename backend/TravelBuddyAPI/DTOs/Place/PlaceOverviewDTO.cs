1eb8fed91a7e3a0565b20a99e34807559905ae94
Mateusz Gazda <matigazda29@gmail.com>
2024-11-30 22:24:52 +0100

    Update place overview and trip point details

--- backend/TravelBuddyAPI/DTOs/Place/PlaceOverviewDTO.cs
+++ backend/TravelBuddyAPI/DTOs/Place/PlaceOverviewDTO.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using TravelBuddyAPI.DTOs.PlaceCategory;
 
 namespace TravelBuddyAPI.DTOs.Place;
 
@@ -18,4 +19,10 @@ public class PlaceOverviewDTO
     public string? State { get; set; }
 
     public string? City { get; set; }
+
+    public string? Street { get; set; }
+
+    public string? HouseNumber { get; set; }
+
+    public PlaceCategoryDTO? SuperCategory { get; set; }
 }
