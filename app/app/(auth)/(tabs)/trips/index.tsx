2730211f4571a6c5a311bfd7dd17dbdc5f681340
AdrianBroniecki <266603@student.pwr.edu.pl>
2024-12-06 02:49:36 +0100

    Change after review #1

--- app/app/(auth)/(tabs)/trips/index.tsx
+++ app/app/(auth)/(tabs)/trips/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import TripBrowseView from "@/views/TripBrowseView";
-import SurveyView from "@/views/SurveyView";
 
 export default function Trips() {
   return <TripBrowseView />;
