72cd901fe968240004b02fe7ab524ffc270c022f
Bartosz Gableta <aksiorex@gmail.com>
2024-12-10 22:37:31 +0100

    Fix passing date and trip and calendar modals from attraction details

--- app/views/AttractionDetailsView.tsx
+++ app/views/AttractionDetailsView.tsx
@@ -6,24 +6,39 @@ import {
   Dimensions,
   ScrollView,
   Linking,
+  FlatList,
 } from "react-native";
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useTheme, Text } from "react-native-paper";
 import { ADD_ICON, DEFAULT_ICON_SIZE, LOCATION_ICON } from "@/constants/Icons";
 import ActionButtons from "@/components/ActionButtons";
 import { MD3ThemeExtended } from "@/constants/Themes";
-import { displayCost, displayTime, formatAddress } from "@/utils/TextUtils";
+import {
+  displayCost,
+  displayTime,
+  formatAddress,
+  truncateText,
+} from "@/utils/TextUtils";
 import StarRatingDisplayComponent from "@/components/StarRatingDisplayComponent";
 import IconComponent from "@/components/IconComponent";
 import { useAttractionDetails } from "@/composables/usePlace";
 import LoadingView from "./LoadingView";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
 import { useSnackbar } from "@/context/SnackbarContext";
 import ConditionIcons from "@/components/ConditionIcons";
 import { CategoryIcons, CategoryLabelsForTripCategory } from "@/types/Profile";
 import { createLocationURL } from "@/utils/maps";
 import { findAttractionCategory } from "@/utils/CategoryUtils";
 import { formatMinutes, getTotalMinutesFromTimestamp } from "@/utils/TimeUtils";
+import CustomModal from "@/components/CustomModal";
+import { RenderItem } from "@/components/RenderItem";
+import ActionTextButtons from "@/components/ActionTextButtons";
+import { useTripDetails } from "@/composables/useTripDetails";
+import { useAuth } from "@/app/ctx";
+import SingleDatePickerModal from "@/components/SingleDatePickerModal";
+import { CalendarDate } from "react-native-paper-dates/lib/typescript/Date/Calendar";
+import { formatDateToISO } from "@/utils/TimeUtils";
+import { TripDay } from "@/types/Trip";
 
 const { height, width } = Dimensions.get("window");
 
@@ -34,8 +49,98 @@ const AttractionDetailsView = () => {
 
   const { showSnackbar } = useSnackbar();
 
+  const { api } = useAuth();
+
   const params = useLocalSearchParams();
   const { place_id, trip_id, day_id, date } = params;
+
+  // Trip and day selection
+  const [selectedTripId, setSelectedTripId] = useState<string | null>(null);
+  const [selectedDayId, setSelectedDayId] = useState<string | null>(null);
+
+  const [currentTrips, setCurrentTrips] = useState<any[]>([]);
+
+  const [isTripSelectorVisible, setIsTripSelectorVisible] = useState(false);
+  const [isDaySelectorVisible, setIsDaySelectorVisible] = useState(false);
+
+  const {
+    tripDetails,
+    loading: tripLoading,
+    error: tripError,
+    refetch: tripFetch,
+  } = useTripDetails(selectedTripId as string, { immediate: false });
+
+  const fetchCurrentTrips = async () => {
+    try {
+      const response = await api!.get("/trips/current");
+      setCurrentTrips(response.data);
+    } catch (error: any) {
+      console.log(error.response.data);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!trip_id) {
+        fetchCurrentTrips();
+      }
+    }, [trip_id]),
+  );
+
+  const handleDismissTripModal = () => {
+    setIsTripSelectorVisible(false);
+    setSelectedTripId(null);
+  };
+
+  const handleTripSelection = (item: any) => {
+    setSelectedTripId(item.id);
+  };
+
+  const handleConfirmTrip = () => {
+    if (selectedTripId) {
+      tripFetch();
+      setIsTripSelectorVisible(false);
+      setIsDaySelectorVisible(true);
+    }
+  };
+
+  const handleDismissDatePicker = useCallback(() => {
+    setIsDaySelectorVisible(false);
+    setSelectedTripId(null);
+    setSelectedDayId(null);
+  }, [setIsDaySelectorVisible]);
+
+  const dateToIdMap = useMemo(() => {
+    return new Map(
+      tripDetails?.tripDays.map((day: TripDay) => [day.date, day.id]) || [],
+    );
+  }, [tripDetails]);
+
+  const handleConfirmDatePicker = useCallback(
+    ({ date }: { date: CalendarDate }) => {
+      const fixedDate = date as Date;
+      const formattedDate = formatDateToISO(fixedDate);
+      const tripDayId = dateToIdMap.get(formattedDate);
+      if (tripDetails?.id && tripDayId) {
+        setIsDaySelectorVisible(false);
+        router.navigate({
+          // @ts-ignore
+          pathname: `/explore/create`,
+          params: {
+            trip_id: tripDetails?.id,
+            day_id: tripDayId,
+            date: fixedDate.toLocaleDateString(),
+            attractionProviderId: place_id,
+          },
+        });
+      } else {
+        console.error("Day not found");
+      }
+    },
+    [setIsDaySelectorVisible, place_id, tripDetails, dateToIdMap],
+  );
+
+  // ---
 
   const { placeDetails, loading, error } = useAttractionDetails(
     place_id as string,
@@ -69,14 +174,18 @@ const AttractionDetailsView = () => {
   };
 
   const onAddButtonPress = () => {
-    router.navigate({
-      // @ts-ignore
-      pathname: `/trips/details/${trip_id}/day/${day_id}/tripPoints/create`,
-      params: {
-        attractionProviderId: place_id,
-        date: date,
-      },
-    });
+    if (trip_id && day_id && date && place_id) {
+      router.navigate({
+        // @ts-ignore
+        pathname: `/trips/details/${trip_id}/day/${day_id}/tripPoints/create`,
+        params: {
+          attractionProviderId: place_id,
+          date: date,
+        },
+      });
+    } else {
+      setIsTripSelectorVisible(true);
+    }
   };
   if (!placeDetails) {
     return <LoadingView transparent={false} />;
@@ -152,6 +261,46 @@ const AttractionDetailsView = () => {
         />
       </View>
       <LoadingView show={loading} transparent={false} />
+      <CustomModal
+        visible={isTripSelectorVisible}
+        onDismiss={handleDismissTripModal}
+      >
+        <Text style={styles.modalHeadline} variant="headlineSmall">
+          Wybierz wycieczkę
+        </Text>
+        <FlatList
+          style={styles.tripList}
+          data={currentTrips}
+          renderItem={({ item }) => (
+            <RenderItem
+              item={item}
+              isSelected={selectedTripId === item.id}
+              onSelect={handleTripSelection}
+              renderContent={(item: any) => truncateText(item.name, 25)}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <View />}
+          ListEmptyComponent={<Text>Brak dostępnych wycieczek</Text>}
+        />
+        <ActionTextButtons
+          onAction1={handleDismissTripModal}
+          onAction2={handleConfirmTrip}
+          action1ButtonLabel="Anuluj"
+          action2ButtonLabel="OK"
+          action1Icon={undefined}
+          action2Icon={undefined}
+        />
+      </CustomModal>
+      {tripDetails && (
+        <SingleDatePickerModal
+          visible={isDaySelectorVisible}
+          startDate={tripDetails?.startDate}
+          endDate={tripDetails?.endDate}
+          onDismiss={handleDismissDatePicker}
+          onConfirm={handleConfirmDatePicker}
+        />
+      )}
     </>
   );
 };
@@ -200,5 +349,13 @@ const createStyles = (theme: MD3ThemeExtended) =>
     },
     starRatingPadding: {
       paddingVertical: 10,
+    },
+    modalHeadline: {
+      alignSelf: "flex-start",
+      marginBottom: 20,
+    },
+    tripList: {
+      maxHeight: 200,
+      marginBottom: 20,
     },
   });
